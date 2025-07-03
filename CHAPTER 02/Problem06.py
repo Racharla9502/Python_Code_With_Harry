79f1f66eba14626e627af0621e743e71dc2ba34f
Racharla Vyshnav Mani Teja <racharlavyshnavmaniteja9502@gmail.com>
2025-07-04 00:37:10 +0530

    Add program to calculate the square root of a number entered by the user

---
+++ CHAPTER 02/Problem06.py
@@ -0,0 +1,9 @@
+# Write a python program to calculate the square of a number entered by the user.
+
+import math # import the math module to use sqrt function
+
+num = float(input("Enter a number: ")) # enter the number in int or float format
+
+square = math.sqrt(num) # calculate the square root of the number
+
+print(f"The square root of the number {num} is: {square}") # print the square root of the number
