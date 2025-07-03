c7771791e9f0dca3d5bb7093b9819b6f785d1433
Racharla Vyshnav Mani Teja <racharlavyshnavmaniteja9502@gmail.com>
2025-07-04 00:29:33 +0530

    Add program to check the type of variable assigned using input function

---
+++ CHAPTER 02/Problem03.py
@@ -0,0 +1,7 @@
+# Check the type of variable assigned using input () function.
+
+a = input("Enter something: ") # enter any data type value
+
+print("The type of variable assigned is:", type(a)) # print the type of variable
+
+# by default the input() function returns the type as string
