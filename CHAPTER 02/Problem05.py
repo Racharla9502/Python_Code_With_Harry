74da6507d9004c723bab766deea9c82744021564
Racharla Vyshnav Mani Teja <racharlavyshnavmaniteja9502@gmail.com>
2025-07-04 00:33:42 +0530

    Add program to find the average of two numbers entered by the user

---
+++ CHAPTER 02/Problem05.py
@@ -0,0 +1,6 @@
+# Write a python program to find an average of two numbers entered by the user.
+
+num1 = float(input("Enter first number: "))  # enter first number
+num2 = float(input("Enter second number: "))  # enter second number
+average = (num1 + num2) / 2  # calculate the average
+print(f"The average of {num1} and {num2} is: {average}")  # print the average
