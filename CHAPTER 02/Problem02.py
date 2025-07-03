d4adcba922b11fd29c7380f069610d4dc8a42047
Racharla Vyshnav Mani Teja <racharlavyshnavmaniteja9502@gmail.com>
2025-07-04 00:26:05 +0530

    Add program to find remainder when a number is divided by another

---
+++ CHAPTER 02/Problem02.py
@@ -0,0 +1,9 @@
+# Write a python program to find remainder when a number is divided by z.
+
+z = int(input("Enter a number: "))  # enter the number
+
+x = int(input("Enter the divisor: "))  # enter the divisor
+
+remainder = x % z  # find the remainder
+
+print(f"The remainder when {z} is divided by {x} is: {remainder}")  # print the remainder
