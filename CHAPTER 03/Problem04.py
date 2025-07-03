fe89dc1bbe22be91051ae51afedceed10be21fce
Racharla Vyshnav Mani Teja <racharlavyshnavmaniteja9502@gmail.com>
2025-07-04 00:55:45 +0530

    Add program to replace double spaces with single spaces in a string

---
+++ CHAPTER 03/Problem04.py
@@ -0,0 +1,4 @@
+# Replace the double space from problem 3 with single spaces.
+string = input("Enter a string: ")
+new_string = string.replace("  ", " ")
+print("String after replacing double spaces:", new_string)
