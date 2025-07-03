729eba6cb4d5b0e97ee8a6b1751e98bb0ee71a95
Racharla Vyshnav Mani Teja <racharlavyshnavmaniteja9502@gmail.com>
2025-07-04 00:54:08 +0530

    Add program to detect double spaces in a string

---
+++ CHAPTER 03/Problem03.py
@@ -0,0 +1,8 @@
+# Write a program to detect double space in a string.
+
+text = input("Enter a string: ")
+
+if "  " in text:
+    print("Double space detected!")
+else:
+    print("No double space found.")
